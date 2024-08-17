c148f86b819fd7c3db48b09ba3ad97ed4d195023
Megha Todakar <meghasjodatti@gmail.com>
2024-08-17 21:21:03 +0800

    Updated code as per little lemon functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import "./App.css";
-import Footer from "./Footer";
-import Header from "./Header";
-import Main from "./Main";
-import Nav from "./Nav";
+import React from 'react';
+import './App.css';
+import Footer from "./components/footer"
+import HomePage from './pages/homePage';
+import Menu from './components/menu';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Nav />
-      <Main />
+    <div>
+      <Menu />
+      <HomePage />
       <Footer />
-    </>
+    </div>
   );
 }
 
